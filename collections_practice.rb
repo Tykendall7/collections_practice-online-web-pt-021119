326d1702dd5fdc4694d094355c8661c57dac6027
Tymeckia Kendall <tykend@gmail.com>
2019-03-06 02:00:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,5 +24,33 @@ def reverse_array(array)
   array
 end
 
+def kesha_maker(array)
+  array.each do |letter|
+    letter[2] = "$"
+  end
+end
 
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+ if array[1] == word
+   word
+else 
+  word + "s"
+end
+end
+end 
   
